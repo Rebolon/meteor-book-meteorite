ac56f0bdc4d65252c777acc135cded0b7bf714f4
rebolon <richard.tribes@gmail.com>
2013-06-11 08:47:45 +0000

    page 85, plus personal hack for latency compensation

--- collections/posts.js
+++ collections/posts.js
@@ -23,6 +23,7 @@ Meteor.methods({
 				'This link has already been posted',
 				postWithSameLink._id);
 		}
+
 		// pick out the whitelisted keys
 		var post = _.extend(_.pick(postAttributes, 'url'), {
 			title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
@@ -30,8 +31,9 @@ Meteor.methods({
 			author: user.username,
 			submitted: new Date().getTime()
 		});
+
 		// wait for 5 seconds
-		if (!this.isSimulation) {
+		if (! this.isSimulation) {
 			var Future = Npm.require('fibers/future');
 			var future = new Future();
 			Meteor.setTimeout(function() {
@@ -41,6 +43,12 @@ Meteor.methods({
 		}
 
 		var postId = Posts.insert(post);
+		/* personal hack coz the code seems to fail because the client views wait for server response to allow callback that will use Router... sad coz latency compensation seems to work but the behavior is not what we want : callback should be called when client side meteor methods post return postId.
+		   so i decided to do the redirection directly here and i ask for more explanation on the github of Microscope project.
+		*/
+		if (this.isSimulation) {
+			Meteor.Router.to('postsList');
+		}
 		return postId;
 	}
 });
